eb2bdba34ca586a2db2d7f4ed96dbb54a53495e7
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-15 21:08:08 -0300

    conectar login com DB e verificar duplicidades

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
+import {UsuarioModel} from '../../models/UsuarioModel';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+import md5 from 'md5';
 
-const endpointCadastro = (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+const endpointCadastro = 
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
     if(req.method === 'POST'){
         const usuario = req.body as CadastroRequisicao;
@@ -21,9 +25,21 @@ const endpointCadastro = (req : NextApiRequest, res : NextApiResponse<RespostaPa
             return res.status(400).json({erro : 'Senha invalida'});
         }
 
-        return res.status(200).json({msg : 'Dados corretos'});
+        //validacao se ja existe usuario com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+            return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
+        }
+        //salvar no banco de dados
+        const usuarioASerSalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha: md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
+        return res.status(200).json({msg : 'Usuario criado com sucesso'});
     }
     return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
