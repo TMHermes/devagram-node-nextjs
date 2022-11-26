651cc55171dcb354344ab98b718c89675b8df030
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-26 18:47:50 -0300

    update endpoint de pesquisa

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -8,11 +8,16 @@ const pesquisaEndpoint =
  async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
 
     try{
-
         if(req.method === 'GET'){
-
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
             const {filtro} = req.query;
-
             if(filtro && filtro.length < 2){
                 return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
             }
@@ -24,6 +29,7 @@ const pesquisaEndpoint =
             });
             
             return res.status(200).json(usuariosEncontrados);
+            }
         }
         return res.status(405).json({erro: 'Metodo informado nao e valido'});
     }catch(e){
