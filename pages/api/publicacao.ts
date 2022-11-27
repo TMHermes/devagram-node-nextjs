ced831b4c056e5c58b9fe6fb4899b3938b04485d
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2022-11-27 19:31:38 -0300

    atualizacao no incremento de publicacoes

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -43,6 +43,9 @@ const handler = nc()
                 data : new Date()
             }
 
+            usuario.publicacoes++;
+            await UsuarioModel.findByIdAndUpdate({_id : usuario._id}, usuario);
+
             await PublicacaoModel.create(publicacao);
             return res.status(200).json({erro : 'Publicacao criada com sucesso'});
         }catch(e){
